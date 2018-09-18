2f006fff0e6ac79eaf8e78d20ccdca85a58084ee
Alvin <31421262+alvinjteng@users.noreply.github.com>
2018-09-18 12:25:53 -0700

    Update Assignment2.cpp
    removed possibility of two consecutive parenthesis.

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -8,7 +8,6 @@ using namespace std;
 int main() {
     string input;
     string connectors = "!V^v->()"; // Includes all Connectors
-    string sConnectors = "!V^v"; // Excludes -> and ()
     string alphabet = "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz"; // Includes the alphabet except 'v' and 'V'
     
     cout << "Enter a WFF: "; // Asks user to enter WFF
@@ -18,8 +17,8 @@ int main() {
     bool lastNot = false;
     bool lastConnector = false;
     bool lastDash = false;
-    bool eLeftP = false;
-    bool lastRightP = false;
+    bool lastLeftP = false;
+    bool lastRightP= false;
     bool isValid = true;
     
     for (auto c: input){
@@ -30,9 +29,9 @@ int main() {
         
         if (connectors.find(c) != string::npos) {
             if (c != '!'){
-                if (!lastAlphabet && !lastDash && c != '(' && !lastRightP) {
+                if (!lastAlphabet && !lastDash && c != '('){
                     isValid = false;
-                }
+               }
                 if (c == '-' && lastAlphabet) { // Checks if c was dash and last was an alphabet character
                     lastDash = true;
                 }
@@ -40,19 +39,14 @@ int main() {
                     lastConnector = true;
                     lastDash = false;
                 }
-                else if (c == '(' && !lastDash) { // Checks for left Parenthetical.
-                    eLeftP = true;
-                    lastConnector = true;
+                else if (c == '(' && !lastDash && !lastConnector && !lastLeftP) { // Checks for left Parenthetical, and no consecutive left parenthesis
+                    lastLeftP = true;
                 }
-                else if (c == ')' && !lastDash && eLeftP){ // Checks for an existing left parenthetical.
+                else if (c == ')' && !lastConnector && !lastDash && !lastRightP && lastLeftP){
                     lastRightP = true;
-                    eLeftP = false;
+                    lastLeftP = false;
                 }
-                else if (sConnectors.find(c) != string::npos && lastRightP) {
-                    lastRightP = false;
-                    lastConnector = true;
-                }
-                else if (sConnectors.find(c) != string::npos) { // Checks c for a connector other than '-' or '>'.
+                else if (connectors.find(c) != string::npos && !lastLeftP && !lastRightP) { // Checks c for a connector other than '-' or '>'.
                     lastConnector = true;
                 }
                 else {
@@ -71,14 +65,18 @@ int main() {
         else if (alphabet.find(c) != string::npos) { // Checks if c is in the alphabet.
             if (lastAlphabet || lastDash){
                 isValid = false;
-            }
+        }
             lastAlphabet = true;
+            lastConnector = false;
             continue;
         }
         else {
             isValid = false;
             break;
         }
+    }
+    if (!lastAlphabet && !lastRightP) {
+        isValid = false;
     }
     cout << (isValid ? "Your WFF is valid." : "Your WFF is invalid.") << endl;
     
