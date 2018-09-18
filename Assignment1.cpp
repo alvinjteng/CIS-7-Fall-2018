f369ebb6da8fd2071a7d6a9a2ebf60782cfa8d99
Alvin <31421262+alvinjteng@users.noreply.github.com>
2018-09-18 12:24:54 -0700

    Update Assignment1.cpp

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
     string input;
-    string connectors = "!V^v->"; // Includes all Connectors
+    string connectors = "!V^v->()"; // Includes all Connectors
     string alphabet = "ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuwxyz"; // Includes the alphabet except 'v' and 'V'
     
     cout << "Enter a WFF: "; // Asks user to enter WFF
@@ -17,6 +17,8 @@ int main() {
     bool lastNot = false;
     bool lastConnector = false;
     bool lastDash = false;
+    bool lastLeftP = false;
+    bool lastRightP= false;
     bool isValid = true;
     
     for (auto c: input){
@@ -27,7 +29,7 @@ int main() {
         
         if (connectors.find(c) != string::npos) {
             if (c != '!'){
-                if (!lastAlphabet && !lastDash){
+                if (!lastAlphabet && !lastDash && c != '('){
                     isValid = false;
                }
                 if (c == '-' && lastAlphabet) { // Checks if c was dash and last was an alphabet character
@@ -37,7 +39,14 @@ int main() {
                     lastConnector = true;
                     lastDash = false;
                 }
-                else if (connectors.find(c) != string::npos) { // Checks c for a connector other than '-' or '>'.
+                else if (c == '(' && !lastDash && !lastConnector && !lastLeftP) { // Checks for left Parenthetical, and no consecutive left parenthesis
+                    lastLeftP = true;
+                }
+                else if (c == ')' && !lastConnector && !lastDash && !lastRightP && lastLeftP){
+                    lastRightP = true;
+                    lastLeftP = false;
+                }
+                else if (connectors.find(c) != string::npos && !lastLeftP && !lastRightP) { // Checks c for a connector other than '-' or '>'.
                     lastConnector = true;
                 }
                 else {
@@ -58,12 +67,16 @@ int main() {
                 isValid = false;
         }
             lastAlphabet = true;
+            lastConnector = false;
             continue;
         }
         else {
             isValid = false;
             break;
         }
+    }
+    if (!lastAlphabet && !lastRightP) {
+        isValid = false;
     }
     cout << (isValid ? "Your WFF is valid." : "Your WFF is invalid.") << endl;
     
